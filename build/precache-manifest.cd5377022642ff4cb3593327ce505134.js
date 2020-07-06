a005f7f6487c218768d9f2fd22ab666de8375537
abhijishaw <abhijishaw88@gmail.com>
2020-07-06 19:03:05 +0530

    new

---
+++ build/precache-manifest.cd5377022642ff4cb3593327ce505134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3256517c81b030f4578fcaffa62579da",
+    "url": "/a04/index.html"
+  },
+  {
+    "revision": "89873aa1b485b983e26f",
+    "url": "/a04/static/css/main.05fcdbfe.chunk.css"
+  },
+  {
+    "revision": "31967128cff3eff49ec8",
+    "url": "/a04/static/js/2.f6dc7953.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/a04/static/js/2.f6dc7953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89873aa1b485b983e26f",
+    "url": "/a04/static/js/main.db311f9a.chunk.js"
+  },
+  {
+    "revision": "54277ddad562998e822a",
+    "url": "/a04/static/js/runtime-main.3a784eb6.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container-fluide App bg-light">
+      <h1>What to do?</h1>
+    </div>
+  );
+}
+
+export default App;
